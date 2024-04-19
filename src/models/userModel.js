67e43950e7123ab5a0233f5e2ab1d98c49ab9ad3
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-04-19 20:24:25 +0530

    folder structure

---
+++ src/models/userModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"Please provide a username"],
+        unique:true
+    },
+    
+})
