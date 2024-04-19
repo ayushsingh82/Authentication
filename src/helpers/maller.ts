d27e8599944fdd0571de4978f10f6b8f26e5bc12
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-04-20 01:14:36 +0530

    nodemailer

---
+++ src/helpers/maller.ts
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+export const sendEmail=async({email,emailType,userId}:any)=>{
+    try{
+              
+const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: "maddison53@ethereal.email",
+      pass: "jn7jnAPss4f63QBp6D",
+    },
+  });
+  
+      const mailOptions=
+        {
+            from: 'hitesh@hitesh.ai', // sender address
+            to: email, // list of receivers
+            subject: emailType==='VERIFY'?"Verify your email": "Reset your password", // Subject line
+            html: "<b>Hello world?</b>", // html body
+          }
+      
+        const mailResponse= await transporter.sendMail(mailOptions)
+        return mailResponse
+
+    }catch(error:any){
+             throw new Error(error.message)
+    }
+}
