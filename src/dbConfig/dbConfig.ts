3910c94c0e49633ce4a1a34a20261c8540281ec5
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-04-19 22:13:56 +0530

    mongodb connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connect (){
+    try{
+           mongoose.connect(process.env.MONGO_URI!)
+           const connection =mongoose.connection
+
+           connection.on('connected',()=>{
+            console.log("Mongodb connected")
+           })
+
+           connection.on('error',(err)=>{
+            console.log("Mongodb connection error" + err) ;
+            process.exit;
+           })
+
+    }catch(error){
+        console.log("Something went wrong in connecting to db");
+        console.log(error)
+    }
+}
